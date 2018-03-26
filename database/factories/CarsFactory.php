30bbe56bdb5be5651f607f30e9e4decfd9b88d8c
Rahul Sharan <rs925@njit.edu>
2018-03-26 01:38:17 -0400

    6. Created a database seed to generate 50 cars.

--- database/factories/CarsFactory.php
+++ database/factories/CarsFactory.php
@@ -2,11 +2,11 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Car::class, function (Faker $faker) {
     return [
-        'Make' => $faker->randomElement($array = array ('FORD','TOYOTA','HONDA'));
-        'Model' => $faker->randomLetter;
-        'Year' => $faker->numberBetween($min =1903 , $max = 2018);
+        'Make' => $faker->randomElement($array = array ('FORD','TOYOTA','HONDA')),
+        'Model' => $faker->randomLetter,
+        'Year' => $faker->numberBetween($min =1903 , $max = 2018),
 
     ];
 });
